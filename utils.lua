7b68dc549e413010f10df6508603a011018a9e87
xsolinsx <ericsolinas@hotmail.it>
2016-09-03 14:20:59 +0200

    fix

--- utils.lua
+++ utils.lua
@@ -4,7 +4,6 @@ https = require "ssl.https"
 ltn12 = require "ltn12"
 
 serpent =(loadfile "./libs/serpent.lua")()
-feedparser =(loadfile "./libs/feedparser.lua")()
 json =(loadfile "./libs/JSON.lua")()
 mimetype =(loadfile "./libs/mimetype.lua")()
 redis =(loadfile "./libs/redis.lua")()
