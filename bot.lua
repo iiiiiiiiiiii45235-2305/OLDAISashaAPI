dea099c277f399c50b87170921ec5cc307b55229
xsolinsx <ericsolinas@hotmail.it>
2016-09-03 15:39:34 +0200

    fix

--- bot.lua
+++ bot.lua
@@ -179,6 +179,7 @@ function load_database()
 end
 
 function bot_init()
+printvardump(clr)
     config = { }
     bot = nil
     plugins = { }
@@ -544,7 +545,7 @@ function match_plugin(plugin, plugin_name, msg)
     for k, pattern in pairs(plugin.patterns) do
         local matches = match_pattern(pattern, msg.text)
         if matches then
-            print(clr.gray .. "msg matches: ", plugin_name, " => ", pattern)
+            print(clr.light_gray .. "msg matches: ", plugin_name, " => ", pattern)
 
             local disabled = is_plugin_disabled_on_chat(plugin_name, msg.receiver)
 
