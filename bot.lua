0a297179fb6ec5ad5790835d86dad3bab7fb4392
xsolinsx <ericsolinas@hotmail.it>
2016-09-05 16:06:42 +0200

    fix print_messages

--- bot.lua
+++ bot.lua
@@ -662,23 +662,23 @@ function print_msg(msg)
         print_text = print_text .. clr.white .. '[reply] ' .. clr.reset
     end
     if msg.media then
-        print_text = print_text .. clr.white .. '[' ..(media_type or 'unsupported') .. '] ' .. clr.reset
+        print_text = print_text .. clr.white .. '[' ..(msg.media_type or 'unsupported') .. '] ' .. clr.reset
         if msg.caption then
             print_text = print_text .. clr.white .. msg.caption .. clr.reset
         end
     end
     if msg.service then
         -- white action reset red name reset
-        if service_type == 'chat_del_user' then
+        if msg.service_type == 'chat_del_user' then
             print_text = print_text .. clr.red ..(msg.remover.first_name ..(msg.remover.last_name or '')) .. clr.reset .. clr.white .. ' deleted user ' ..(msg.removed.first_name ..(msg.removed.last_name or '')) .. clr.reset
-        elseif service_type == 'chat_del_user_leave' then
+        elseif msg.service_type == 'chat_del_user_leave' then
             print_text = print_text .. clr.red ..(msg.remover.first_name ..(msg.remover.last_name or '')) .. clr.reset .. clr.white .. ' left the chat ' .. clr.reset
-        elseif service_type == 'chat_add_user' then
+        elseif msg.service_type == 'chat_add_user' then
             print_text = print_text .. clr.red ..(msg.adder.first_name ..(msg.adder.last_name or '')) .. clr.reset .. clr.white .. ' added user ' ..(msg.added.first_name ..(msg.added.last_name or '')) .. clr.reset
-        elseif service_type == 'chat_add_user_link' then
+        elseif msg.service_type == 'chat_add_user_link' then
             print_text = print_text .. clr.red ..(msg.adder.first_name ..(msg.adder.last_name or '')) .. clr.reset .. clr.white .. ' joined chat by invite link ' .. clr.reset
         else
-            print_text = print_text .. clr.white .. '[' ..(service_type or 'unsupported') .. '] ' .. clr.reset
+            print_text = print_text .. clr.white .. '[' ..(msg.service_type or 'unsupported') .. '] ' .. clr.reset
         end
     end
     if msg.text then
