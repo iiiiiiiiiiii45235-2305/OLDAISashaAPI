6f6f8303c2ecb4afe07f742ac6ac279b76529e8d
xsolinsx <ericsolinas@hotmail.it>
2016-09-25 15:17:37 +0200

    fix fakerank

--- plugins/fakecommand.lua
+++ plugins/fakecommand.lua
@@ -5,7 +5,6 @@ local function run(msg, matches)
         mystat('/fakecommand')
         local rank = get_rank(msg.from.id, msg.chat.id)
         local fakerank = rank_table[matches[1]:upper()]
-        print(rank, fakerank)
         if fakerank <= rank then
             -- yes
             -- remove "[#!/]<rank> " from message so it's like a normal message
@@ -14,7 +13,12 @@ local function run(msg, matches)
             msg.text = msg.text:gsub('/' .. matches[1] .. ' ', '')
             -- replace the id of the executer with a '*' followed by the rank value so when it's checked with (i.e.) is_mod(msg) bot knows it's a fakecommand
             msg.from.id = '*' .. rank_table[matches[1]:upper()]
-            match_plugins(msg)
+            if msg_valid(msg) then
+                msg = pre_process_msg(msg)
+                if msg then
+                    match_plugins(msg)
+                end
+            end
         else
             -- no
             return langs[msg.lang].fakecommandYouTried
