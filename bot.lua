12e1ecec17b78c115c33bd7f5e1ce3e2bbe64a8e
xsolinsx <ericsolinas@hotmail.it>
2016-09-27 01:13:13 +0200

    fix

--- bot.lua
+++ bot.lua
@@ -228,27 +228,39 @@ end
 
 function update_redis_cron()
     if redis:get('last_redis_cron') then
-        last_redis_cron = redis:hget('last_redis_cron')
+        if redis:get('last_redis_cron') ~= os.date('%M') then
+            local value = os.date('%M')
+            redis:set('last_redis_cron', value)
+        end
+        last_redis_cron = redis:get('last_redis_cron')
     else
         local value = os.date('%M')
         redis:set('last_redis_cron', value)
-        last_redis_cron = value
+        last_redis_cron = redis:get('last_redis_cron')
     end
 
     if redis:get('last_redis_db_cron') then
-        last_redis_db_cron = redis:hget('last_redis_db_cron')
+        if redis:get('last_redis_db_cron') ~= os.date('%H') then
+            local value = os.date('%H')
+            redis:set('last_redis_db_cron', value)
+        end
+        last_redis_db_cron = redis:get('last_redis_db_cron')
     else
         local value = os.date('%H')
         redis:set('last_redis_db_cron', value)
-        last_redis_db_cron = value
+        last_redis_db_cron = redis:get('last_redis_db_cron')
     end
 
     if redis:get('last_redis_administrator_cron') then
-        last_redis_administrator_cron = redis:hget('last_redis_administrator_cron')
+        if redis:get('last_redis_administrator_cron') ~= os.date('%d') then
+            local value = os.date('%d')
+            redis:set('last_redis_administrator_cron', value)
+        end
+        last_redis_administrator_cron = redis:get('last_redis_administrator_cron')
     else
         local value = os.date('%d')
         redis:set('last_redis_administrator_cron', value)
-        last_redis_administrator_cron = value
+        last_redis_administrator_cron = redis:get('last_redis_administrator_cron')
     end
 end
 
