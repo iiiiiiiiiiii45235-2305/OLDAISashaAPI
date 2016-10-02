389cfe61485aa255acef829554101921159c75b9
xsolinsx <ericsolinas@hotmail.it>
2016-10-02 17:07:27 +0200

    not working, rollback

--- utils.lua
+++ utils.lua
@@ -427,9 +427,9 @@ function match_pattern(pattern, text, lower_case)
     if text then
         local matches = { }
         if lower_case then
-            matches = { string.match(text:lower(), pattern) or string.match(text:lower():gsub('@[Aa][Ii][Ss][Aa][Ss][Hh][Aa][Bb][Oo][Tt]', ''), pattern) }
+            matches = { string.match(text:lower(), pattern) }
         else
-            matches = { string.match(text, pattern) or string.match(text:gsub('@[Aa][Ii][Ss][Aa][Ss][Hh][Aa][Bb][Oo][Tt]', ''), pattern) }
+            matches = { string.match(text, pattern) }
         end
         if next(matches) then
             return matches
