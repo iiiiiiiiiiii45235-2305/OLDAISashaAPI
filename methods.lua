af9a7460095a1b624bc980b3cdac6e99ca26b0d6
xsolinsx <ericsolinas@hotmail.it>
2016-11-13 19:29:15 +0100

    adjust warnUser

--- methods.lua
+++ methods.lua
@@ -1040,7 +1040,15 @@ function warnUser(executer, target, chat_id)
         if tonumber(warn_chat) ~= 0 then
             if tonumber(hashonredis) >= tonumber(warn_chat) then
                 redis:getset(chat_id .. ':warn:' .. target, 0)
-                banUser(executer, target, chat_id)
+                savelog(chat_id, "[" .. executer .. "] warned user " .. target .. " Y")
+                if data[tostring(chat_id)] then
+                    if data[tostring(chat_id)].settings then
+                        if data[tostring(chat_id)].settings.strict then
+                            return banUser(executer, target, chat_id)
+                        end
+                    end
+                end
+                return kickUser(executer, target, chat_id)
             end
             sendMessage(chat_id, string.gsub(langs[lang].warned, 'X', tostring(hashonredis)))
         end
