a25e9c9874e42b8488ce813f4699f293e063eed3
xsolinsx <ericsolinas@hotmail.it>
2017-01-08 20:47:40 +0100

    fix

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -3,6 +3,7 @@ local function get_msgs_user_chat(user_id, chat_id)
     local user_info = { }
     local uhash = 'user:' .. user_id
     local user = redis:hgetall(uhash)
+    printvardump(user)
     local um_hash = 'msgs:' .. user_id .. ':' .. chat_id
     user_info.msgs = tonumber(redis:get(um_hash) or 0)
     user_info.name = user.first_name ..(user.last_name or '') .. ' [' .. user_id .. ']'
