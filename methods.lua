7e54d14aafb4567773fc0c00f2b78ac4e1d35315
xsolinsx <ericsolinas@hotmail.it>
2017-02-10 21:36:30 +0100

    fix

--- methods.lua
+++ methods.lua
@@ -1331,13 +1331,13 @@ function print_msg(msg, dont_print)
             end
             if msg.service then
                 if msg.service_type == 'chat_del_user' then
-                    print_text = print_text .. clr.red ..(msg.remover.first_name ..(msg.remover.last_name or '')) .. clr.reset .. clr.blue .. ' deleted user ' .. clr.reset .. clr.red ..((msg.removed.first_name or '$Deleted Account$') ..(msg.removed.last_name or '')) .. ' ' .. clr.reset
+                    print_text = print_text .. clr.red ..((msg.remover.first_name or 'ERROR') ..(msg.remover.last_name or '')) .. clr.reset .. clr.blue .. ' deleted user ' .. clr.reset .. clr.red ..((msg.removed.first_name or '$Deleted Account$') ..(msg.removed.last_name or '')) .. ' ' .. clr.reset
                 elseif msg.service_type == 'chat_del_user_leave' then
-                    print_text = print_text .. clr.red ..(msg.remover.first_name ..(msg.remover.last_name or '')) .. clr.reset .. clr.blue .. ' left the chat ' .. clr.reset
+                    print_text = print_text .. clr.red ..((msg.remover.first_name or 'ERROR') ..(msg.remover.last_name or '')) .. clr.reset .. clr.blue .. ' left the chat ' .. clr.reset
                 elseif msg.service_type == 'chat_add_user' then
-                    print_text = print_text .. clr.red ..(msg.adder.first_name ..(msg.adder.last_name or '')) .. clr.reset .. clr.blue .. ' added user ' .. clr.reset .. clr.red ..(msg.added.first_name ..(msg.added.last_name or '')) .. ' ' .. clr.reset
+                    print_text = print_text .. clr.red ..((msg.adder.first_name or 'ERROR') ..(msg.adder.last_name or '')) .. clr.reset .. clr.blue .. ' added user ' .. clr.reset .. clr.red ..(msg.added.first_name ..(msg.added.last_name or '')) .. ' ' .. clr.reset
                 elseif msg.service_type == 'chat_add_user_link' then
-                    print_text = print_text .. clr.red ..(msg.adder.first_name ..(msg.adder.last_name or '')) .. clr.reset .. clr.blue .. ' joined chat by invite link ' .. clr.reset
+                    print_text = print_text .. clr.red ..((msg.adder.first_name or 'ERROR') ..(msg.adder.last_name or '')) .. clr.reset .. clr.blue .. ' joined chat by invite link ' .. clr.reset
                 else
                     print_text = print_text .. clr.blue .. '[' ..(msg.service_type or 'unsupported service') .. '] ' .. clr.reset
                 end
