43bbb48274f53e9735f0c45f2c670641bac7baa1
xsolinsx <ericsolinas@hotmail.it>
2017-02-10 19:11:25 +0100

    move print_msg

--- bot.lua
+++ bot.lua
@@ -781,56 +781,6 @@ local function is_plugin_disabled_on_chat(plugin_name, chat_id)
     return false
 end
 
-function print_msg(msg, dont_print)
-    if msg then
-        if not msg.printed then
-            msg.printed = true
-            local hour = os.date('%H')
-            local minute = os.date('%M')
-            local second = os.date('%S')
-            local chat_name = msg.chat.title or(msg.chat.first_name ..(msg.chat.last_name or ''))
-            local sender_name = msg.from.title or(msg.from.first_name ..(msg.from.last_name or ''))
-            local print_text = clr.cyan .. ' [' .. hour .. ':' .. minute .. ':' .. second .. ']  ' .. chat_name .. ' ' .. clr.reset .. clr.red .. sender_name .. clr.reset .. clr.blue .. ' >>> ' .. clr.reset
-            if msg.edited then
-                print_text = print_text .. clr.blue .. '[edited] ' .. clr.reset
-            end
-            if msg.forward then
-                print_text = print_text .. clr.blue .. '[forward] ' .. clr.reset
-            end
-            if msg.reply then
-                print_text = print_text .. clr.blue .. '[reply] ' .. clr.reset
-            end
-            if msg.media then
-                print_text = print_text .. clr.blue .. '[' ..(msg.media_type or 'unsupported media') .. '] ' .. clr.reset
-                if msg.caption then
-                    print_text = print_text .. clr.blue .. msg.caption .. clr.reset
-                end
-            end
-            if msg.service then
-                if msg.service_type == 'chat_del_user' then
-                    print_text = print_text .. clr.red ..(msg.remover.first_name ..(msg.remover.last_name or '')) .. clr.reset .. clr.blue .. ' deleted user ' .. clr.reset .. clr.red ..((msg.removed.first_name or '$Deleted Account$') ..(msg.removed.last_name or '')) .. ' ' .. clr.reset
-                elseif msg.service_type == 'chat_del_user_leave' then
-                    print_text = print_text .. clr.red ..(msg.remover.first_name ..(msg.remover.last_name or '')) .. clr.reset .. clr.blue .. ' left the chat ' .. clr.reset
-                elseif msg.service_type == 'chat_add_user' then
-                    print_text = print_text .. clr.red ..(msg.adder.first_name ..(msg.adder.last_name or '')) .. clr.reset .. clr.blue .. ' added user ' .. clr.reset .. clr.red ..(msg.added.first_name ..(msg.added.last_name or '')) .. ' ' .. clr.reset
-                elseif msg.service_type == 'chat_add_user_link' then
-                    print_text = print_text .. clr.red ..(msg.adder.first_name ..(msg.adder.last_name or '')) .. clr.reset .. clr.blue .. ' joined chat by invite link ' .. clr.reset
-                else
-                    print_text = print_text .. clr.blue .. '[' ..(msg.service_type or 'unsupported service') .. '] ' .. clr.reset
-                end
-            end
-            if msg.text then
-                print_text = print_text .. clr.blue .. msg.text .. clr.reset
-            end
-            if not dont_print then
-                print(msg.chat.id)
-                print(print_text)
-            end
-            return print_text
-        end
-    end
-end
-
 function match_plugin(plugin, plugin_name, msg)
     -- Go over patterns. If one matches it's enough.
     for k, pattern in pairs(plugin.patterns) do
