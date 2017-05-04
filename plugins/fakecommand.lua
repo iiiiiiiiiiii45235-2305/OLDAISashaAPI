c81da44b78ec4f923e771fad6f3414f1fbac9b84
xsolinsx <ericsolinas@hotmail.it>
2017-05-04 23:15:10 +0200

    fix

--- plugins/fakecommand.lua
+++ plugins/fakecommand.lua
@@ -15,9 +15,9 @@ local function run(msg, matches)
             -- replace the id of the executer with a '*' followed by the rank value so when it's checked with (i.e.) is_mod(msg) bot knows it's a fakecommand
             copied_msg.from.id = '*' .. rank_table[matches[1]:upper()]
             copied_msg.from.tg_cli_id = '*' .. rank_table[matches[1]:upper()]
-            print(copied_msg.from.id, copied_msg.text)
+            copied_msg.from.is_mod = false
+            copied_msg.from.is_owner = false
             copied_msg = get_tg_rank(copied_msg)
-            printvardump(copied_msg)
             if msg_valid(copied_msg) then
                 match_plugins(copied_msg)
             end
