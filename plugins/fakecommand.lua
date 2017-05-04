e59a5bebb5cf46692d34d8fbdf53b5fe96efb6f3
xsolinsx <ericsolinas@hotmail.it>
2017-05-04 20:20:23 +0200

    test

--- plugins/fakecommand.lua
+++ plugins/fakecommand.lua
@@ -14,6 +14,7 @@ local function run(msg, matches)
             copied_msg.text = copied_msg.text:gsub('/' .. matches[1] .. ' ', '')
             -- replace the id of the executer with a '*' followed by the rank value so when it's checked with (i.e.) is_mod(msg) bot knows it's a fakecommand
             copied_msg.from.id = '*' .. rank_table[matches[1]:upper()]
+            print(copied_msg.from.id, copied_msg.text)
             if msg_valid(copied_msg) then
                 match_plugins(copied_msg)
             end
