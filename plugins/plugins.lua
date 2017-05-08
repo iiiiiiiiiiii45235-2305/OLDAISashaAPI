e818e99d17f87cfbd21d20258837674bea009700
xsolinsx <ericsolinas@hotmail.it>
2017-05-08 20:49:09 +0200

    fix

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -20,7 +20,26 @@ end
 
 -- Returns true if it is a system plugin
 local function system_plugin(p)
-    if p == 'administrator' or p == 'anti_spam' or p == 'banhammer' or p == 'bot' or p == 'broadcast' or p == 'check_tag' or p == 'database' or p == 'feedback' or p == 'goodbyewelcome' or p == 'group_management' or p == 'info' or p == 'msg_checks' or p == 'onservice' or p == 'plugins' or p == 'strings' or p == 'tgcli_to_api_migration' or p == 'whitelist' then
+    if p == 'administrator' or
+        p == 'anti_spam' or
+        p == 'banhammer' or
+        p == 'bot' or
+        p == 'broadcast' or
+        p == 'check_tag' or
+        p == 'database' or
+        p == 'feedback' or
+        p == 'filemanager' or
+        p == 'goodbyewelcome' or
+        p == 'group_management' or
+        p == 'info' or
+        p == 'lua_exec' or
+        p == 'msg_checks' or
+        p == 'onservice' or
+        p == 'plugins' or
+        p == 'strings' or
+        p == 'tgcli_to_api_migration' or
+        p == 'todo' or
+        p == 'whitelist' then
         return true
     end
     return false
@@ -415,6 +434,7 @@ return {
         "^(###cbplugins)(DISABLE)(.*)(%-%d+)$",
         "^(###cbplugins)(ENABLE)(.*)$",
         "^(###cbplugins)(DISABLE)(.*)$",
+
         "^[#!/]([Pp][Ll][Uu][Gg][Ii][Nn][Ss])$",
         "^[#!/]([Pp][Ll][Uu][Gg][Ii][Nn][Ss]) ([Cc][Hh][Aa][Tt])$",
         "^[#!/]([Tt][Ee][Xx][Tt][Uu][Aa][Ll][Pp][Ll][Uu][Gg][Ii][Nn][Ss])$",
