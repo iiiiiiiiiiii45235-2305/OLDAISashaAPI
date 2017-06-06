d455ef3a4f5322df76ebc06fea6b3c753a8c049f
xsolinsx <ericsolinas@hotmail.it>
2017-06-06 20:38:45 +0200

    fix

--- plugins/anti_spam.lua
+++ plugins/anti_spam.lua
@@ -127,7 +127,7 @@ local function pre_process(msg)
                         end
                         local text = ''
                         if string.match(getWarn(msg.chat.id), "%d+") then
-                            text = warnUser(bot.id, msg.from.id, msg.chat.id, langs[msg.lang].reasonFlood)
+                            text = tostring(warnUser(bot.id, msg.from.id, msg.chat.id, langs[msg.lang].reasonFlood))
                             text = text .. '\n' .. tostring(kickUser(bot.id, msg.from.id, msg.chat.id, langs[msg.lang].reasonFlood))
                         elseif not strict then
                             text = kickUser(bot.id, msg.from.id, msg.chat.id, langs[msg.lang].reasonFlood)
