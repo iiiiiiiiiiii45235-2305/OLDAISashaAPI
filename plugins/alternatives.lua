9f9c9bf53883017d8a2b5c0b95d98a06c8400197
xsolinsx <ericsolinas@hotmail.it>
2017-07-04 00:33:36 +0200

    fix

--- plugins/alternatives.lua
+++ plugins/alternatives.lua
@@ -326,10 +326,10 @@ local function pre_process(msg)
                     for k, v in pairs(alternatives[tostring(msg.chat.id)].altCmd) do
                         if msg.media then
                             local file_id = ''
-                            if msg.reply_to_message.media_type == 'photo' then
+                            if msg.media_type == 'photo' then
                                 local bigger_pic_id = ''
                                 local size = 0
-                                for k, v in pairsByKeys(msg.reply_to_message.photo) do
+                                for k, v in pairsByKeys(msg.photo) do
                                     if v.file_size then
                                         if v.file_size > size then
                                             size = v.file_size
@@ -338,20 +338,20 @@ local function pre_process(msg)
                                     end
                                 end
                                 file_id = bigger_pic_id
-                            elseif msg.reply_to_message.media_type == 'video' then
-                                file_id = msg.reply_to_message.video.file_id
-                            elseif msg.reply_to_message.media_type == 'video_note' then
-                                file_id = msg.reply_to_message.video_note.file_id
-                            elseif msg.reply_to_message.media_type == 'audio' then
-                                file_id = msg.reply_to_message.audio.file_id
-                            elseif msg.reply_to_message.media_type == 'voice_note' then
-                                file_id = msg.reply_to_message.voice.file_id
-                            elseif msg.reply_to_message.media_type == 'gif' then
-                                file_id = msg.reply_to_message.document.file_id
-                            elseif msg.reply_to_message.media_type == 'document' then
-                                file_id = msg.reply_to_message.document.file_id
-                            elseif msg.reply_to_message.media_type == 'sticker' then
-                                file_id = msg.reply_to_message.sticker.file_id
+                            elseif msg.media_type == 'video' then
+                                file_id = msg.video.file_id
+                            elseif msg.media_type == 'video_note' then
+                                file_id = msg.video_note.file_id
+                            elseif msg.media_type == 'audio' then
+                                file_id = msg.audio.file_id
+                            elseif msg.media_type == 'voice_note' then
+                                file_id = msg.voice.file_id
+                            elseif msg.media_type == 'gif' then
+                                file_id = msg.document.file_id
+                            elseif msg.media_type == 'document' then
+                                file_id = msg.document.file_id
+                            elseif msg.media_type == 'sticker' then
+                                file_id = msg.sticker.file_id
                             else
                                 return msg
                             end
@@ -371,10 +371,10 @@ local function pre_process(msg)
                     for k, v in pairs(alternatives.global.altCmd) do
                         if msg.media then
                             local file_id = ''
-                            if msg.reply_to_message.media_type == 'photo' then
+                            if msg.media_type == 'photo' then
                                 local bigger_pic_id = ''
                                 local size = 0
-                                for k, v in pairsByKeys(msg.reply_to_message.photo) do
+                                for k, v in pairsByKeys(msg.photo) do
                                     if v.file_size then
                                         if v.file_size > size then
                                             size = v.file_size
@@ -383,20 +383,20 @@ local function pre_process(msg)
                                     end
                                 end
                                 file_id = bigger_pic_id
-                            elseif msg.reply_to_message.media_type == 'video' then
-                                file_id = msg.reply_to_message.video.file_id
-                            elseif msg.reply_to_message.media_type == 'video_note' then
-                                file_id = msg.reply_to_message.video_note.file_id
-                            elseif msg.reply_to_message.media_type == 'audio' then
-                                file_id = msg.reply_to_message.audio.file_id
-                            elseif msg.reply_to_message.media_type == 'voice_note' then
-                                file_id = msg.reply_to_message.voice.file_id
-                            elseif msg.reply_to_message.media_type == 'gif' then
-                                file_id = msg.reply_to_message.document.file_id
-                            elseif msg.reply_to_message.media_type == 'document' then
-                                file_id = msg.reply_to_message.document.file_id
-                            elseif msg.reply_to_message.media_type == 'sticker' then
-                                file_id = msg.reply_to_message.sticker.file_id
+                            elseif msg.media_type == 'video' then
+                                file_id = msg.video.file_id
+                            elseif msg.media_type == 'video_note' then
+                                file_id = msg.video_note.file_id
+                            elseif msg.media_type == 'audio' then
+                                file_id = msg.audio.file_id
+                            elseif msg.media_type == 'voice_note' then
+                                file_id = msg.voice.file_id
+                            elseif msg.media_type == 'gif' then
+                                file_id = msg.document.file_id
+                            elseif msg.media_type == 'document' then
+                                file_id = msg.document.file_id
+                            elseif msg.media_type == 'sticker' then
+                                file_id = msg.sticker.file_id
                             else
                                 return msg
                             end
