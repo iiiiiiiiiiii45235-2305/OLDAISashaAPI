1cc75c63d1ce8d625a08dc19aaf24b2c2603a3c7
xsolinsx <ericsolinas@hotmail.it>
2017-07-31 22:23:44 +0200

    fix

--- plugins/fakecommand.lua
+++ plugins/fakecommand.lua
@@ -1,24 +1,25 @@
 -- no "SUDO" because there's no rank higher than "SUDO" (yes there's "BOT" but it's not a real rank)
 -- "USER" can't use this because there's no rank lower than "USER"
 local function run(msg, matches)
-    local rank = get_rank(msg.from.id, msg.chat.id, true)
-    if rank > 0 then
+    if msg.from.is_mod then
+        local rank = get_rank(msg.from.id, msg.chat.id, true)
         local fakerank = rank_table[matches[1]:upper()]
         if fakerank <= rank then
             -- yes
             mystat('/fakecommand')
             -- remove "[#!/]<rank> " from message so it's like a normal message
-            msg.text = msg.text:gsub('#' .. matches[1] .. ' ', '')
-            msg.text = msg.text:gsub('!' .. matches[1] .. ' ', '')
-            msg.text = msg.text:gsub('/' .. matches[1] .. ' ', '')
+            local copied_msg = clone_table(msg)
+            copied_msg.text = copied_msg.text:gsub('#' .. matches[1] .. ' ', '')
+            copied_msg.text = copied_msg.text:gsub('!' .. matches[1] .. ' ', '')
+            copied_msg.text = copied_msg.text:gsub('/' .. matches[1] .. ' ', '')
             -- replace the id of the executer with a '*' followed by the rank value so when it's checked with (e.g.) is_mod(msg) bot knows it's a fakecommand
-            msg.from.id = '*' .. rank_table[matches[1]:upper()]
-            msg.from.tg_cli_id = '*' .. rank_table[matches[1]:upper()]
-            msg.from.is_mod = false
-            msg.from.is_owner = false
-            msg = get_tg_rank(msg)
-            if msg_valid(msg) then
-                match_plugins(msg)
+            copied_msg.from.id = '*' .. rank_table[matches[1]:upper()]
+            copied_msg.from.tg_cli_id = '*' .. rank_table[matches[1]:upper()]
+            copied_msg.from.is_mod = false
+            copied_msg.from.is_owner = false
+            copied_msg = get_tg_rank(copied_msg)
+            if msg_valid(copied_msg) then
+                match_plugins(copied_msg)
             end
             msg = nil
         else
