7e06f6496446e5f440b1b69d59ba643ec4882de4
xsolinsx <ericsolinas@hotmail.it>
2017-09-03 13:02:19 +0200

    test

--- plugins/test.lua
+++ plugins/test.lua
@@ -1,6 +1,7 @@
 local function run(msg, matches)
     if is_sudo(msg) then
         io.popen("lua timework.lua 30 40 \"porco dio\"")
+        io.popen("lua timework.lua 40 \"porco dio\" 30")
     end
 end
 
