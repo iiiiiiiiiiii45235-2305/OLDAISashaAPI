856f740e71653506fe56ecd5db4c1db84b47cd54
xsolinsx <ericsolinas@hotmail.it>
2017-09-03 13:44:00 +0200

    test

--- timework.lua
+++ timework.lua
@@ -2,5 +2,5 @@ require("introtimework")
 local _bot, _sudoers, a, b = ...
 sendMessage(41400331, vardumptext(_bot:gsub("\\n", " "):gsub('\\"', '"')) .. '\n' .. vardumptext(_sudoers:gsub("\\n", " "):gsub('\\"', '"')) .. '\n' .. a .. b)
 bot = assert(loadstring(_bot:gsub("\\n", " "):gsub('\\"', '"')))()
-sudoers = assert(loadstring(_sudoers:gsub("\n", " "):gsub('\\"', '"')))()
+sudoers = assert(loadstring(_sudoers:gsub("\\n", " "):gsub('\\"', '"')))()
 sendMessage(41400331, vardumptext(bot) .. '\n' .. vardumptext(sudoers) .. '\n' .. a .. b)
