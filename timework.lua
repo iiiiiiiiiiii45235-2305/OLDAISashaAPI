04a69f38981ae1c1058ada578d12de4560d72c90
xsolinsx <ericsolinas@hotmail.it>
2017-09-03 12:34:20 +0200

    test

--- timework.lua
+++ timework.lua
@@ -1,3 +1,4 @@
+require("bot")
 local a, b, c, d = ...
 os.execute("sleep " .. 3)
 printvardump(a)
@@ -5,3 +6,4 @@ printvardump(b)
 printvardump(c)
 os.execute("sleep " .. 10)
 printvardump(d(a))
+printvardump(vardumptext(a))
