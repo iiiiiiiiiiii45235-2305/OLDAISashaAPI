dad3b8b31355ffdd6bb1b393497429c47cccb76b
xsolinsx <ericsolinas@hotmail.it>
2017-09-04 10:58:07 +0200

    test

--- timework.lua
+++ timework.lua
@@ -1,4 +1,13 @@
 require("introtimework")
-local a, b = ...
-sendMessage(41400331, vardumptext(_bot) .. '\n' .. vardumptext(_sudoers) .. '\n' .. a .. b)
-bot = assert(loadstring(_bot))()
+local sleep_time, string_to_execute, chat_id = ...
+os.execute('sleep ' .. sleep_time)
+sendMessage(41400331, sleep_time .. string_to_execute)
+local output = loadstring(string_to_execute)()
+if output then
+    if type(output) == 'table' then
+        output = nil
+    end
+end
+if output then
+    sendMessage(chat_id, tostring(output))
+end
