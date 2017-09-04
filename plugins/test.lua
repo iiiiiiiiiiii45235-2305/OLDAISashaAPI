4f42f33bfc9f1ef454959156a2628eaa04b49c2f
xsolinsx <ericsolinas@hotmail.it>
2017-09-04 11:10:25 +0200

    test

--- plugins/test.lua
+++ plugins/test.lua
@@ -1,12 +1,12 @@
 local function run(msg, matches)
     if is_sudo(msg) then
-        io.popen('lua timework.lua "' .. matches[2] .. '" "' .. matches[3] .. '" "' .. matches[4] .. '"')
+        io.popen('lua timework.lua "' .. matches[1] .. '" "' .. matches[2] .. '" "' .. matches[3] .. '"')
     end
 end
 
 return {
     description = "TEST",
-    patterns = { "^[#!/]([Tt][Ee][Ss][Tt]) (%d+) (^%s) (%-?%d+)$", },
+    patterns = { "^[#!/][Tt][Ee][Ss][Tt] (%d+) ([^%s]+) (%-?%d+)$", },
     run = run,
     min_rank = 4,
     syntax = { }
