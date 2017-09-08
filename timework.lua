4f84a59cc0acf524ac18f80948463f715febb007
xsolinsx <ericsolinas@hotmail.it>
2017-09-08 02:04:03 +0200

    fix

--- timework.lua
+++ timework.lua
@@ -1,6 +1,6 @@
 loadfile("./introtimework.lua")()
 local action, chat_id, sleep_time = ...
-if sleep_time > 0 then
+if tonumber(sleep_time) > 0 then
     os.execute('sleep ' .. sleep_time)
 end
 if action == 'spamtext' then
