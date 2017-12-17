5505f482c7a6131d5e4c3e5fede523601adc2916
xsolinsx <ericsolinas@hotmail.it>
2017-12-17 19:52:20 +0100

    fix

--- keyboards.lua
+++ keyboards.lua
@@ -966,6 +966,7 @@ function get_object_info_keyboard(executer, obj, chat_id, deeper)
                             if is_executer_mod or is_mod2(executer, chat_id, true) then
                                 if not userInChat(chat_id, obj.id, true) then
                                     row = row + 1
+                                    keyboard.inline_keyboard[row] = { }
                                     keyboard.inline_keyboard[row][column] = { text = '📨 INVITE 📨', callback_data = 'infoINVITE' .. obj.id .. chat_id }
                                 end
                                 if is_executer_owner or is_owner2(executer, chat_id, true) then
