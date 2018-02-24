eeea9fbbb38e4d871907ac1a570ea0b31cac8fe0
xsolinsx <ericsolinas@hotmail.it>
2018-02-24 01:11:55 +0100

    fix

--- keyboards.lua
+++ keyboards.lua
@@ -526,10 +526,10 @@ function keyboard_settings_list(chat_id, page, from_other_plugin)
     else
         keyboard.inline_keyboard[row][2] = { text = '☑️ ' .. reverseGroupDataDictionary['pmnotices'], callback_data = 'group_managementLOCK' .. reverseGroupDataDictionary['pmnotices'] .. page .. chat_id }
     end
+    row = row + 1
+    keyboard.inline_keyboard[row] = { }
     if data[tostring(chat_id)] then
         if tonumber(page) == 1 then
-            row = row + 1
-            keyboard.inline_keyboard[row] = { }
             keyboard.inline_keyboard[row][1] = { text = langs[lang].infoEmoji .. reverseGroupDataDictionary['lock_grouplink'], callback_data = 'group_management' .. reverseGroupDataDictionary['lock_grouplink'] }
             keyboard.inline_keyboard[row][2] = { text = langs[lang].infoEmoji .. reverseGroupDataDictionary['lock_name'], callback_data = 'group_management' .. reverseGroupDataDictionary['lock_name'] }
             keyboard.inline_keyboard[row][3] = { text = langs[lang].infoEmoji .. reverseGroupDataDictionary['lock_photo'], callback_data = 'group_management' .. reverseGroupDataDictionary['lock_photo'] }
@@ -567,28 +567,12 @@ function keyboard_settings_list(chat_id, page, from_other_plugin)
             end
             keyboard.inline_keyboard[row][1] = { text = langs[lang].gotoMutes, callback_data = 'group_managementGOTOMUTES' .. chat_id }
         elseif tonumber(page) == 2 then
-            local column = 1
-            local flag = false
-            row = row + 1
-            keyboard.inline_keyboard[row] = { }
-            keyboard.inline_keyboard[row + 1] = { }
             for var, value in pairsByKeys(data[tostring(chat_id)].settings.mutes) do
-                if flag then
-                    flag = false
-                    row = row + 2
-                    column = 1
-                    keyboard.inline_keyboard[row] = { }
-                    keyboard.inline_keyboard[row + 1] = { }
-                end
-                keyboard.inline_keyboard[row][column] = { text = langs[lang].infoEmoji .. reverseGroupDataDictionary[var:lower()], callback_data = 'group_management' .. reverseGroupDataDictionary[var:lower()] }
-                keyboard.inline_keyboard[row + 1][column] = { text = reverse_punishments_table_emoji[value] .. reverse_punishments_table[value], callback_data = 'group_management' .. reverseGroupDataDictionary[var:lower()] .. page .. chat_id }
-                column = column + 1
-                if column > 3 then
-                    flag = true
-                end
+                keyboard.inline_keyboard[row][1] = { text = langs[lang].infoEmoji .. reverseGroupDataDictionary[var:lower()], callback_data = 'group_management' .. reverseGroupDataDictionary[var:lower()] }
+                keyboard.inline_keyboard[row][2] = { text = reverse_punishments_table_emoji[value] .. reverse_punishments_table[value], callback_data = 'group_management' .. reverseGroupDataDictionary[var:lower()] .. page .. chat_id }
+                row = row + 1
+                keyboard.inline_keyboard[row] = { }
             end
-            row = row + 2
-            keyboard.inline_keyboard[row] = { }
             keyboard.inline_keyboard[row][1] = { text = langs[lang].gotoLocks, callback_data = 'group_managementGOTOLOCKS' .. chat_id }
         end
         row = row + 1
