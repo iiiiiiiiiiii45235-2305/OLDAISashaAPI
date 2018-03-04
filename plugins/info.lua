76503fe68cef8dca57158bfe79431706b3ba06fd
xsolinsx <ericsolinas@hotmail.it>
2018-03-04 17:21:46 +0100

    fix

--- plugins/info.lua
+++ plugins/info.lua
@@ -1367,10 +1367,16 @@ return {
         "^(###cbinfo)(DELETE)(%-%d+)$",
         "^(###cbinfo)(DELETE)(%-?%d+)(%-%d+)$",
         "^(###cbinfo)(BACK)(%-%d+)$",
+        -- A
+        "^(###cbinfo)(DELETE)(%-?%d+)(%-?%d+)$",
+        -- B
         "^(###cbinfo)(LINK)(%-%d+)$",
         "^(###cbinfo)(NEWLINK)(%-%d+)$",
         "^(###cbinfo)(DELETE)(%d+)(%-%d+)$",
         "^(###cbinfo)(BACK)(%-?%d+)(%-%d+)$",
+        -- A
+        "^(###cbinfo)(BACK)(%-?%d+)(%-?%d+)$",
+        -- B
         "^(###cbinfo)(INVITE)(%d+)(%-%d+)$",
         "^(###cbinfo)(WHITELIST)(%d+)(%-%d+)$",
         "^(###cbinfo)(WHITELISTGBAN)(%d+)(%-%d+)$",
