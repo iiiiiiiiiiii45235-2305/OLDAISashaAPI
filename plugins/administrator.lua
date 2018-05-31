a389600d4558ab376dc9f482fe923feda3eb5332
xsolinsx <ericsolinas@hotmail.it>
2018-05-31 18:27:44 +0200

    broadcast with random delay to prevent flood_wait

--- plugins/administrator.lua
+++ plugins/administrator.lua
@@ -884,7 +884,7 @@ local function run(msg, matches)
             if matches[1] == 'broadcast' then
                 mystat('/broadcast')
                 for k, v in pairs(data['groups']) do
-                    sendMessage(v, matches[2])
+                    io.popen('lua timework.lua "sendmessage" "' .. math.random(#data['groups']) .. '" "' .. v .. '" "nil" "' .. matches[2] .. '"')
                 end
                 return
             end
