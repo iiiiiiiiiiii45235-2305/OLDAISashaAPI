0f6fdc7c2ee451a0dffc5e0a18727db62141ebb0
xsolinsx <ericsolinas@hotmail.it>
2018-06-03 18:47:17 +0200

    test

--- plugins/file_conversion.lua
+++ plugins/file_conversion.lua
@@ -33,20 +33,20 @@ local function run(msg, matches)
             local res = getFile(file_id)
             local download_link = telegram_file_link(res)
             local file_path, res_code = download_to_file(download_link, "/home/pi/AISashaAPI/data/tmp/" .. file_name)
-            --[[if msg.reply_to_message.media_type == 'photo' then
-                file_name = msg.reply_to_message.photo.file_name or msg.reply_to_message.photo.file_id
-                file_id = msg.reply_to_message.photo.file_id
+            if msg.reply_to_message.media_type == 'photo' then
+                return sendDocument(msg.chat.id, file_path)
+            elseif msg.reply_to_message.media_type == 'video' then
+                return sendDocument(msg.chat.id, file_path)
             elseif msg.reply_to_message.media_type == 'video_note' then
-                file_name = msg.reply_to_message.video_note.file_name or msg.reply_to_message.video_note.file_id
-                file_id = msg.reply_to_message.video_note.file_id
-            else]]if msg.reply_to_message.media_type == 'video' then
-                return sendVideoNote(msg.chat.id, file_path)
+                return sendDocument(msg.chat.id, file_path)
             elseif msg.reply_to_message.media_type == 'audio' then
                 return sendVoice(msg.chat.id, file_path)
             elseif msg.reply_to_message.media_type == 'voice_note' then
                 return sendAudio(msg.chat.id, file_path)
             elseif msg.reply_to_message.media_type == 'gif' then
-                return sendVideoNote(msg.chat.id, file_path)
+                return sendDocument(msg.chat.id, file_path)
+            elseif msg.reply_to_message.media_type == 'sticker' then
+                return sendDocument(msg.chat.id, file_path)
             else
                 return langs[msg.lang].useQuoteOnFile
             end
