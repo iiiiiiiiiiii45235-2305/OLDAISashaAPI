ec579a83375814dfac8060cf99b6a7f71a436666
xsolinsx <ericsolinas@hotmail.it>
2018-07-27 22:52:34 +0200

    fix

--- plugins/tex.lua
+++ plugins/tex.lua
@@ -1,9 +1,7 @@
 local function run(msg, matches)
-    local eq = URL.escape(matches[1])
-    local url = "http://latex.codecogs.com/png.download?" ..
-    "\\dpi{300}%20\\LARGE%20" .. eq
+    local url = "http://latex.codecogs.com/png.download?\\dpi{300}%20\\LARGE%20" .. URL.escape(matches[1])
+    print(url)
     io.popen('lua timework.lua "fileconversion" "0" "' .. msg.chat.id .. '" "' .. url .. '" "data/tmp/' .. math.random() .. '" "photo"')
-    return pyrogramUpload(msg.chat.id, "photo", url, msg.message_id)
 end
 
 return {
