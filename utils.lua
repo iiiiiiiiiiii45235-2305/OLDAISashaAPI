783cb5d8299bc78d76afc74eb2adf6d7ff27cb6f
xsolinsx <ericsolinas@hotmail.it>
2018-07-30 12:05:54 +0200

    test

--- utils.lua
+++ utils.lua
@@ -637,7 +637,7 @@ end
 function executeBackupCommand(tar_command, time)
     print("Executing " .. tar_command)
     local backup_name = string.match(tar_command, 'backup(.*)%d+%.tar%.gz')
-    local log = assert(io.popen('cd "/home/pi/BACKUPS/" && ' .. tar_command):read('*all'))
+    local log = assert(io.popen('cd "/home/pi/BACKUPS/" && ' .. tar_command .. ' && cd "/home/pi/AISashaAPI/"'):read('*all'))
     local file_backup_log = io.open("/home/pi/BACKUPS/backupLog" .. time .. ".txt", "w")
     file_backup_log:write(log)
     file_backup_log:flush()
