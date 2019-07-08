2a6a0539d2a485bc9a98f4e75400e4050eae1656
xsolinsx <ericsolinas@hotmail.it>
2019-07-08 19:30:49 +0200

    fix

--- pyrogramThings.py
+++ pyrogramThings.py
@@ -44,7 +44,7 @@ try:
             file_path = ""
         app.download_media(message=ffile, file_name=file_path)
         if text != "":
-            app.send_chat_action(chat_id=chat_id, action="typing)
+            app.send_chat_action(chat_id=chat_id, action="typing")
             app.send_message(chat_id=chat_id, text=text)
     elif method == "UPLOAD":
         media_type = str(sys.argv[3])
@@ -60,31 +60,31 @@ try:
             caption = ""
         try:
             if media_type == "audio":
-                app.send_chat_action(chat_id=chat_id, action="upload_audio)
+                app.send_chat_action(chat_id=chat_id, action="upload_audio")
                 app.send_audio(chat_id=chat_id, audio=ffile, reply_to_message_id=reply_id, caption=caption)
             elif media_type == "document":
-                app.send_chat_action(chat_id=chat_id, action="upload_document)
+                app.send_chat_action(chat_id=chat_id, action="upload_document")
                 app.send_document(chat_id=chat_id, document=ffile, reply_to_message_id=reply_id, caption=caption)
             elif media_type == "gif":
-                app.send_chat_action(chat_id=chat_id, action="upload_video)
+                app.send_chat_action(chat_id=chat_id, action="upload_video")
                 app.send_animation(chat_id=chat_id, animation=ffile, reply_to_message_id=reply_id, caption=caption)
             elif media_type == "photo":
-                app.send_chat_action(chat_id=chat_id, action="upload_photo)
+                app.send_chat_action(chat_id=chat_id, action="upload_photo")
                 app.send_photo(chat_id=chat_id, photo=ffile, reply_to_message_id=reply_id, caption=caption)
             elif media_type == "sticker":
                 app.send_sticker(chat_id=chat_id, sticker=ffile, reply_to_message_id=reply_id)
             elif media_type == "video":
-                app.send_chat_action(chat_id=chat_id, action="record_video)
+                app.send_chat_action(chat_id=chat_id, action="record_video")
                 app.send_video(chat_id=chat_id, video=ffile, reply_to_message_id=reply_id, caption=caption)
             elif media_type == "video_note":
-                app.send_chat_action(chat_id=chat_id, action="record_video_note)
+                app.send_chat_action(chat_id=chat_id, action="record_video_note")
                 app.send_video_note(chat_id=chat_id, video_note=ffile, reply_to_message_id=reply_id)
             elif media_type == "voice_note":
-                app.send_chat_action(chat_id=chat_id, action="record_audio)
+                app.send_chat_action(chat_id=chat_id, action="record_audio")
                 app.send_voice(chat_id=chat_id, voice=ffile, reply_to_message_id=reply_id, caption=caption)
             else:
                 #default
-                app.send_chat_action(chat_id=chat_id, action="upload_document)
+                app.send_chat_action(chat_id=chat_id, action="upload_document")
                 app.send_document(chat_id=chat_id, document=ffile, reply_to_message_id=reply_id, caption=caption)
         except Exception as e:
             app.send_chat_action(chat_id=chat_id, action="typing")
